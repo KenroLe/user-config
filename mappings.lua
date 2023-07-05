f155c20dfb6e2b742543830705dc40245c7145dd
Kenro <kenro2002@gmail.com>
2023-07-05 12:26:07 +0000

    dadbod: fix broken config and added keymaps

--- mappings.lua
+++ mappings.lua
@@ -7,15 +7,19 @@ return {
   -- first key is the mode
   n = {
     ["<leader>t"] = false,
+    ["<leader>e"] = false,
     ["<S-k>"] = { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" },
     ["<S-j>"] = { function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "Previous buffer"},
     ["<leader>x"] = {function() require("astronvim.utils.buffer").close() end, desc = "Close current buffer"},
     ["<leader>tt"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm float" },
     ["<leader>tj"] = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "ToggleTerm horizontal split" },
     ["<leader>tl"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "ToggleTerm vertical split" },
+    ["<leader>ed"] = {"<cmd>DBUIToggle<cr>", desc= "Toggle Database Explorer"},
+    ["<leader>ee"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" }
+
   },
   t = {
     -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
+    ["<esc>"] = false,
   },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,18 +1,10 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
   {
     "kristijanhusak/vim-dadbod-ui",
-    event = 'dadbod',
-    lazy = false,
-    config = function() require('vim-dadbod-ui').setup(opts) end
+    lazy = false
+  },
+  {
+    "tpope/vim-dadbod",
+    lazy= false
   }
 }
