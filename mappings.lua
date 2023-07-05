76912576a002f0d6193ff0411efd201c82731d92
Kenro <kenro2002@gmail.com>
2023-07-05 13:47:58 +0000

    tabbing comments

--- mappings.lua
+++ mappings.lua
@@ -11,11 +11,14 @@ return {
     ["<S-k>"] = { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" },
     ["<S-j>"] = { function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "Previous buffer"},
     ["<leader>x"] = {function() require("astronvim.utils.buffer").close() end, desc = "Close current buffer"},
+    -- terminal
     ["<leader>tt"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm float" },
     ["<leader>tj"] = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "ToggleTerm horizontal split" },
     ["<leader>tl"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "ToggleTerm vertical split" },
+    -- explorers
     ["<leader>ed"] = {"<cmd>DBUIToggle<cr>", desc= "Toggle Database Explorer"},
     ["<leader>ee"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
+    -- tab management
     ["<leader>wk"] = { function() vim.cmd.tabnext() end, desc = "Next tab" },
     ["<leader>wj"] = { function() vim.cmd.tabprevious() end, desc = "Previous tab" },
     ["<leader>wn"] = { function() vim.cmd.tabnew() end, desc = "New tab" },
